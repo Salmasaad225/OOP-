34513d7e1dadc38cc56cbfe5a8e5e438fda600e8
Salma Saad <123007642+Salmasaad225@users.noreply.github.com>
2023-04-26 23:28:00 +0200

    Add files via upload

---
+++ lab 6/lab 6 .2/Program.cs
@@ -0,0 +1,145 @@
+﻿using lab_6_._2;
+using System;
+
+ namespace lab_6_._2
+{
+
+    public class squere
+    {
+        public double x { set; get; }
+
+        public squere(squere squere)   // copy constructor
+        {
+            this.x = squere.x;
+        }
+
+        public squere(double _x)    //constructor with pramiters
+        {
+           x = _x;
+        }
+
+        public squere()    //constructor without pramiters
+        {
+            x = 2;
+        }
+        public double area()
+        {
+            double res = Math.Pow(x, 2);
+            return res;
+        }
+
+
+    }
+
+    class rectangle
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public double area()
+        {
+            double res = x * y;
+            return res;
+        }
+
+    }
+
+    class circle
+    {
+        public double c { get; set; }
+
+
+        public double area()
+        {
+            double res =(22/7) * Math.Pow(c, 2);
+            return res;
+        }
+    }
+
+
+
+    class Picture
+    {
+        squere s;
+        rectangle r;
+
+        public squere pro_s                      // property   composition
+        {
+            set
+            {
+                s = new squere(value);
+            }
+            get
+            {
+                return new squere(s);
+            }
+        }
+
+
+        //public rectangle pro_r { get; set; }     
+
+        public rectangle pro_r      /* property  aggregation*/
+        {
+            set
+            {
+                r = value;
+            }
+            get
+            {
+                return r;
+            }
+        }
+
+
+
+
+        public double gettotalarea()
+        {
+           
+            return r.area() + s.area();
+        }
+
+        public void circlearea(circle c)
+        {
+            Console.WriteLine($"the circle area is {c.area()}");
+
+        }
+    }
+
+
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        squere ss = new squere(3);
+        
+        rectangle r = new rectangle();
+        r.x = 2;
+        r.y = 3;
+        
+
+
+        circle c = new circle();
+        c.c = 5;
+
+
+
+        Picture p = new Picture();
+        p.pro_s = ss;
+        p.pro_r = r;
+        p.circlearea(c);
+        Console.WriteLine($"total area of rectangle and sqare is {p.gettotalarea()}");
+
+
+
+
+
+
+    } 
+}
---
+++ lab 6/lab 6/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace lab_6
+{
+
+    class shape
+    {
+        protected double dim;        //public with child    private with main
+
+        public double pro_dim
+        {
+            set
+            {
+                dim = value;
+            }
+
+            get
+            {
+                return dim;
+            }
+        }
+
+
+        public double area()
+        {
+            return 0;
+        }
+
+    }
+
+    class squere : shape
+    {
+
+        public double area()
+        {
+            double res = Math.Pow(dim, 2);
+            return res;
+        }
+    }
+
+    class rectangle : shape
+    {
+        public double dim2 { get; set; }
+
+
+
+        public double area()
+        {
+            double res = dim * dim2;
+            return res;
+        }
+    }
+
+    class circle : shape
+    {
+
+        public double area()
+        {
+            double res = (22 / 7) * Math.Pow(dim, 2);
+            return res;        //باي * نق اس 2
+        }
+    }
+
+
+
+
+
+    class Program
+     {
+        static void Main(string[] args)
+        {
+            circle c = new circle();
+            c.pro_dim = 2;
+            Console.WriteLine($"the area of circle is {c.area()}");
+
+            rectangle r = new rectangle();
+            r.pro_dim = 2;
+            r.dim2 = 3;
+            Console.WriteLine($"the area of rectangle is {r.area()}");
+
+            squere s = new squere();
+            s.pro_dim = 2;
+            Console.WriteLine($"the area of squere is {s.area()}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
