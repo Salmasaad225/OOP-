0f27c599718a58ab8425e8b688716ff9caf33d62
Salma Saad <123007642+Salmasaad225@users.noreply.github.com>
2023-04-26 23:22:00 +0200

    Add files via upload

---
+++ lab 4/lab4/Program.cs
@@ -0,0 +1,168 @@
+﻿using DocumentFormat.OpenXml.Office2013.PowerPoint.Roaming;
+using NetTopologySuite.Algorithm;
+using System;
+
+namespace lab4
+{
+     class Student
+    {
+        string name;
+        int age;
+        string password;
+        string username;
+
+
+        public string Name
+        {
+            set
+            {
+                if (value.Length > 5)
+                {
+                    name = value;
+                }
+            }
+
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                if (value > 20)
+                {
+                    age = value;
+                }
+            }
+
+            get
+            {
+                return age;
+            }
+        }
+
+        public string Adress { get; set; }
+        public int Grade { get; set; }
+
+        public string Password
+        {  
+
+         set
+            {
+                if (value.ToString().Length > 8)
+                {
+                    password = value;
+                }
+            }
+        private get
+            {
+                return password;
+            }
+
+            
+        }
+
+        public string Username
+        {
+            set
+            {
+                username = name + age;
+            }
+            get
+            {
+                string ss =name +age;
+                return  ss;
+            }
+        }
+
+           public void forgetpassword (string uname)
+        {
+           if(username == uname)
+            {
+                Console.WriteLine("enter your pass agin");
+                password = Console.ReadLine();
+            }
+
+            else
+            {
+
+                throw new Exception("error .. you are not the real user ");
+            }
+        }
+
+    }     
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+
+            Console.WriteLine(" please enter your name");
+            stu.Name = Console.ReadLine();
+
+            Console.WriteLine(" please enter your age");
+            stu.Age =int.Parse( Console.ReadLine());
+
+            Console.WriteLine(" please enter your adress");
+            stu.Adress = Console.ReadLine();
+
+            Console.WriteLine(" please enter your grade");
+            stu.Grade =int.Parse( Console.ReadLine());
+
+            Console.WriteLine(" please enter your password");
+            stu.Password = Console.ReadLine();
+
+            Console.WriteLine("forget password ?  yes or now ");
+            string pp = Console.ReadLine();
+
+            if (pp == "yes")
+            {
+                Console.WriteLine("plese enter your user name ");
+                string uname = Console.ReadLine();
+
+
+                 
+               
+                    try
+                    {
+                        stu.forgetpassword(uname);
+                    }
+
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+
+
+                    }
+                
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+       
+    }
+}
---
+++ lap 3/lab 3/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace lab_3
+{
+    class Program
+
+
+    {
+        static int[,] filldata()           /*-----------------111111111------------------*/
+        {
+            int[,] twodim = new int[3, 4];
+
+            for (int c = 0; c < twodim.GetLength(1); c++)
+            {
+                for (int r = 0; r < twodim.GetLength(0); r++)
+                {
+                    Console.WriteLine($"please enter student {c + 1} of grade {r + 1}");
+                    twodim[r, c] = int.Parse(Console.ReadLine());
+                }
+            }
+
+
+            return twodim;
+
+
+        }
+        static void printdata(int[,]arr)      /* --------------------111111111111111111---------------*/
+        {
+            
+
+            for (int c = 0; c <arr.GetLength(1); c++)
+            {
+                for (int r = 0; r < arr.GetLength(0); r++)
+                {
+                    Console.WriteLine(arr[r,c]);
+                   
+                }
+            }
+
+
+
+        }
+
+        static void printsum()           /*-----------------2222222222222------------------*/
+        {
+            int[,] twodim = new int[3, 4];
+            int[] sum = new int[4];
+
+            for (int c = 0; c < twodim.GetLength(1); c++)
+            {
+
+
+                for (int r = 0; r < twodim.GetLength(0); r++)
+                {
+                    Console.WriteLine($"please enter student {c + 1} of grade {r + 1}");
+                    twodim[r, c] = int.Parse(Console.ReadLine());
+
+
+                }
+
+                int ss = 0;
+                for (int r = 0; r < twodim.GetLength(0); r++)
+                {
+
+                    sum[c] = twodim[r, c];
+                    ss += sum[c];
+
+
+
+                }
+                Console.WriteLine(ss);
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            ////1.Fill and Print a 2D Array(Using Functions)
+
+
+
+            int[,] arr = new int[3, 4];
+
+            arr = filldata();
+
+            printdata(arr);
+
+
+
+
+
+            //2.Sum of every 'Column' in 2D Array(Using Functions)
+
+
+
+            //printsum();
+
+
+
+            //3.Simple Menu on Array(Refer to Problem 6 in Lab 2)(2D Array Bonus)(Using Functions)
+
+            int[] array = new int[5];
+
+            Console.WriteLine("please enter one of the words add , edit , delete , desplay or exite");
+            string word = Console.WriteLine();
+
+
+            if(word=="exite")
+            {
+                break;
+            }
+
+
+
+            while (word != "exite")
+            {
+                switch (word)
+                {
+                    case "add":
+                        Console.WriteLine("please enter the value");
+                        int Value = int.Parse(Console.ReadLine());
+                        add();
+                        break;
+                    case "edit":
+                       edit();
+                        break;
+                    case "delete":
+                        delete();
+                        break;
+                    case "display":
+                        display();
+                        break;
+                    default:
+                        Console.WriteLine(" try again");
+                        break;
+
+                }  
+
+            }
+
+
+            static void add(int[] array, int value)
+            {
+                int c = 0;
+
+                try
+                {
+                    array[c + 1] = value;
+                    c++;
+                    Console.WriteLine("succeed");
+                }
+
+                catch
+                {
+                    Console.WriteLine("the array falled try agin !!!");
+                }
+                
+                
+            }
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+    }
+}
+
